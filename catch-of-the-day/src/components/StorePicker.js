ea06680f76443c32e8d2582f76155877f5042f9e
VChet <17050347+VChet@users.noreply.github.com>
2018-02-05 19:01:02 +0500

    React event and history.push

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,31 @@ import React from "react";
 import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // stop form submitting
+    event.preventDefault();
+    // get the text from the box
+    const storeName = this.myInput.value.value;
+    // change the page from / to /store/:storeId
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store →</button>
       </form>
-    )
+    );
   }
 }
 
