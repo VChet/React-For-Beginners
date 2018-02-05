a31abe786b1a181d5e75717356a80049d01275be
VChet <17050347+VChet@users.noreply.github.com>
2018-02-05 18:23:02 +0500

    Routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import Router from "./components/Router";
 import "./css/style.css";
-import App from "./components/App";
-import StorePicker from "./components/StorePicker";
 
-render(<App/>, document.querySelector("#main"));
+render(<Router />, document.querySelector("#main"));
