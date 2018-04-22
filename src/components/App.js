97a4b0ec34f7f437ad8d00af463f6485f06306f0
VChet <17050347+VChet@users.noreply.github.com>
2018-04-22 21:17:26 +0500

    Fish form and it's state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    // take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // add new fishes
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
