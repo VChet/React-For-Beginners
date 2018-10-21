2ae1f2d602d1ca91982c51363c936b862cf7d860
VChet <17050347+VChet@users.noreply.github.com>
2018-10-21 23:01:26 +0500

    Add fish to order on click

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,19 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     // set the new fishes object to state
     this.setState({ fishes });
-  }
+  };
 
   loadSamplesFishes = () => {
     this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order }
+    // 2. Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
   }
 
   render() {
@@ -30,7 +39,7 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
           </ul>
         </div>
         <Order />
